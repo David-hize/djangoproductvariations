d606cf3f1a3746b443c66aa06adb47c700e410ed
David-hize <56909853+David-hize@users.noreply.github.com>
2019-11-13 21:31:24 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,20 @@
-from django.urls import path
-from core.views import (
-	HomeView,
-	ItemDetailView,
-	OrderSummaryView,
-	CheckoutView,
-	add_to_cart,
-	remove_from_cart,
-	remove_single_item_from_cart
-)
-
-app_name = 'core'
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-	path('', HomeView.as_view(), name='homepage'),
-	path('checkout/', CheckoutView.as_view(), name='checkout'),
-	path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-	path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-	path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-	path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-	path('remove-item-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
-
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls', namespace='core')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+# 	import debug_toolbar
+# 	urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
